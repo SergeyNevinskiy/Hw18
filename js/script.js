e2a92041aabefdd82c883376e2140375bc856a86
SergeyNevinskiy <88575089+SergeyNevinskiy@users.noreply.github.com>
2021-12-01 22:04:47 +0200

    Hw18

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // function mainFunction (callback) {
 // 	const a = +prompt("Enter 1 number");
 // 	const b = +prompt("Enter 2 number ");
-
 // 	// return exponentiation(a, b);
 // 	// return multiplay(a, b);
 // 	// return division(a, b);
@@ -9,28 +8,24 @@
 // }
 
 // function exponentiation (a, b) {
-
 // 	const result = Math.pow(a, b);
 // 	alert(result);
 // 	return result;
 // }
 
 // function multiplay (a, b) {
-
 // 	const result = a * b;
 // 	alert(result);
 // 	return result;
 // }
 
 // function division (a, b) {
-
 // 	const result = (a / b);
 // 	alert(result);
 // 	return result;
 // }
 
 // function modulo (a, b) {
-
 // 	const result = a % b;
 // 	alert(result);
 // 	return result;
