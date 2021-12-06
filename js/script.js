b93a6e95d18c6762586205e27f3fd95bc4f056bf
SergeyNevinskiy <88575089+SergeyNevinskiy@users.noreply.github.com>
2021-12-06 12:31:59 +0200

    Hw18

--- js/script.js
+++ js/script.js
@@ -1,37 +1,34 @@
-// function mainFunction (callback) {
-// 	const a = +prompt("Enter 1 number");
-// 	const b = +prompt("Enter 2 number ");
-// 	// return exponentiation(a, b);
-// 	// return multiplay(a, b);
-// 	// return division(a, b);
-// 	// return modulo(a, b);
-// }
+function mainFunction(callback) {
+  const a = +prompt("Enter 1 number");
+  const b = +prompt("Enter 2 number ");
+  return callback(a, b);
+}
 
-// function exponentiation (a, b) {
-// 	const result = Math.pow(a, b);
-// 	alert(result);
-// 	return result;
-// }
+function exponentiation(a, b) {
+  const result = a ** b;
+  alert(result);
+  return result;
+}
 
-// function multiplay (a, b) {
-// 	const result = a * b;
-// 	alert(result);
-// 	return result;
-// }
+function multiplay(a, b) {
+  const result = a * b;
+  alert(result);
+  return result;
+}
 
-// function division (a, b) {
-// 	const result = (a / b);
-// 	alert(result);
-// 	return result;
-// }
+function division(a, b) {
+  const result = a / b;
+  alert(result);
+  return result;
+}
 
-// function modulo (a, b) {
-// 	const result = a % b;
-// 	alert(result);
-// 	return result;
-// }
+function modulo(a, b) {
+  const result = a % b;
+  alert(result);
+  return result;
+}
 
-// console.log(mainFunction(exponentiation));
-// console.log(mainFunction(multiplay));
-// console.log(mainFunction(division));
-// console.log(mainFunction(modulo));
+console.log(mainFunction(exponentiation));
+console.log(mainFunction(multiplay));
+console.log(mainFunction(division));
+console.log(mainFunction(modulo));
